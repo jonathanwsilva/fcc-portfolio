baa7647b69081ae8afec69c98b00aeca5a823d5e
Jon Silva <jon.silva.codes@gmail.com>
2018-07-09 19:49:47 -0400

    Clean up comments

--- scripts/script.js
+++ scripts/script.js
@@ -24,22 +24,3 @@ navLinks.addEventListener("click", function(event){
     targetSection.focus();
 
 });
-
-
-// navLinks.addEventListener("click", function(event){
-//     let target = document.getElementById("Experience");
-
-//     //Keep the browser from jumping immediately to the target element
-//     event.preventDefault();
-
-//     //Create a scrolloptions object to act as scrollTo argument
-//     let scrollOptions = Object.create(null);
-//     scrollOptions.top = target.offsetTop;
-//     scrollOptions.behavior = "smooth";
-//     window.scrollTo(scrollOptions);
-
-//     //User might have expected us to jump to target element.
-//     //For accessinility reasons, focus on target element 
-//     target.focus();
-
-// });
